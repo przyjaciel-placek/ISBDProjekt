1676958467c444a0944ec66fabb87fb47f203cc4
przyjaciel-placek <80282702+przyjaciel-placek@users.noreply.github.com>
2022-11-08 03:21:42 +0100

    jakieś głupoty do bazy dopisałem xD

--- Pracownik_app/models.py
+++ Pracownik_app/models.py
@@ -8,4 +8,37 @@ class Pracownik(models.Model):
     ImieP = models.CharField(max_length=30)
     NazwiskoP = models.CharField(max_length=30)
     PeselP = models.CharField(max_length=11)
+    DataZatr = models.DateField()                                   # argumenty??
+    HasloP = models.CharField(max_length=64)
     
+class Klient(models.Model):
+    LoginK = models.CharField(primary_key=True, max_length=20)
+    ImieK = models.CharField(max_length=30)
+    NazwiskoK = models.CharField(max_length=30)
+    HasloK = models.CharField(max_length=64)
+    AdresK = models.CharField(max_length=80)
+
+class Ksiazka(models.Model):                                        # ???
+    IdKs = models.PositiveIntegerField(primary_key=True)
+    TytulKs = models.CharField(max_length=150)
+    AutorKs = models.CharField(max_length=255)
+    CenaKs = models.FloatField()
+
+class Zamowienie(models.Model):
+    IdZ = models.PositiveIntegerField(primary_key=True)             # autonumeracja do dodania
+    DataZ = models.DateField()                                      # argumenty??
+    Status = models.CharField(max_length=30)
+    LoginK = Klient.LoginK                                          # ???????
+    IdKs = Ksiazka.IdKs                                             # ???????
+
+class Recenzja(models.Model):
+    IdR = models.IntegerField(primary_key=True)
+    TrescR = models.CharField(max_length=2000)
+    DataWyst = models.DateField()
+    LoginK = Klient.LoginK
+    IdKs = models.IntegerField()
+
+class Obserwacja(models.Model):
+    IdO = models.PositiveIntegerField(primary_key=True)
+    LoginK = Klient.LoginK
+    IdKs = Ksiazka.IdKs
